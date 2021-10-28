1c433274d0ec7104ef1dfbb6413297138afdd0cd
Sayali Gaikawad <gaiksaya@amazon.com>
2021-10-28 15:20:50 -0700

    Add license
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- lib/auditing/ci-audit-logging.ts
+++ lib/auditing/ci-audit-logging.ts
@@ -1,3 +1,11 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ */
+
 import { Bucket, BucketAccessControl } from '@aws-cdk/aws-s3';
 import { Duration, Stack } from '@aws-cdk/core';
 
