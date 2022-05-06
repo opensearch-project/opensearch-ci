4cf6d291787e944c521ac1e7863e274c6a10696e
pgodithi <pgodithi@amazon.com>
2022-05-06 11:29:04 -0400

    Added assume role feature
    Signed-off-by: pgodithi <pgodithi@amazon.com>

--- bin/ci-stack.ts
+++ bin/ci-stack.ts
@@ -9,7 +9,6 @@
 import { App, RemovalPolicy } from '@aws-cdk/core';
 import { CIStack } from '../lib/ci-stack';
 import { CIConfigStack } from '../lib/ci-config-stack';
-import { DeployAwsAssets } from '../lib/deploy-aws-assets';
 
 const app = new App();
 
@@ -19,6 +18,3 @@ const ciConfigStack = new CIConfigStack(app, `OpenSearch-CI-Config-${defaultEnv}
 
 const ciStack = new CIStack(app, `OpenSearch-CI-${defaultEnv}`, {});
 
-new DeployAwsAssets(app, `OpenSearch-CI-Deploy-Assets-${defaultEnv}`, {
-    envName: defaultEnv,
-});
