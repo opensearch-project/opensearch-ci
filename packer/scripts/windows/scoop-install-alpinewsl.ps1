c6aa391e60d93f94babc019ae84cf06eccd506b2
Peter Zhu <zhujiaxi@amazon.com>
2022-07-13 21:41:23 -0400

    Add initial windows packer templates for ami
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

---
+++ packer/scripts/windows/scoop-install-alpinewsl.ps1
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# The OpenSearch Contributors require contributions made to
+# this file be licensed under the Apache-2.0 license or a
+# compatible open source license.
+
+# Disable "current" alias directory as it is not preserved after AMI creation
+# Use static path in environment variable
+scoop config NO_JUNCTIONS true
+
+# Install alpine
+scoop bucket add extras
+scoop install alpinewsl
---
+++ packer/scripts/windows/userdata.ps1
@@ -0,0 +1,26 @@
+<powershell>
+
+############################## With HTTP ######################################
+## Setup winrm
+cmd /c winrm quickconfig -q
+cmd /c winrm quickconfig '-transport:http'
+cmd /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
+cmd /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
+cmd /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
+cmd /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
+cmd /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
+cmd /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
+cmd /c winrm set "winrm/config/listener?Address=*+Transport=HTTP" '@{Port="5985"}'
+cmd /c netsh advfirewall firewall set rule group="remote administration-winrm" new enable=yes
+cmd /c netsh firewall add portopening TCP 5985 "Port 5985"
+cmd /c netsh advfirewall firewall set rule group="remote administration-smb" new enable=yes
+cmd /c netsh firewall add portopening TCP 445 "Port 445"
+cmd /c net stop winrm
+cmd /c sc config winrm start= auto
+cmd /c net start winrm
+
+echo "Create Jenkins Home"
+New-Item -Path 'C:\\Users\\Administrator\\jenkins' -ItemType Directory
+
+</powershell>
+
