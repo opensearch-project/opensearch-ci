c921275d7cdbb94f1affded6d4ef8c0eda831b11
Peter Zhu <zhujiaxi@amazon.com>
2022-08-24 18:48:01 -0400

    Fix Ubuntu agent start up on userdata (#191)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- lib/compute/agent-nodes.ts
+++ lib/compute/agent-nodes.ts
@@ -11,180 +11,178 @@ import { Stack } from '@aws-cdk/core';
 import { AgentNodeProps } from './agent-node-config';
 
 export class AgentNodes {
-    // Refer: https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/ec2/model/InstanceType.html for instance types
-    readonly AL2_X64: AgentNodeProps;
+  // Refer: https://docs.aws.amazon.com/AWSJavaSDK/latest/javadoc/com/amazonaws/services/ec2/model/InstanceType.html for instance types
+  readonly AL2_X64: AgentNodeProps;
 
-    readonly AL2_X64_DOCKER_HOST: AgentNodeProps;
+  readonly AL2_X64_DOCKER_HOST: AgentNodeProps;
 
-    readonly AL2_X64_DOCKER_HOST_PERF_TEST: AgentNodeProps;
+  readonly AL2_X64_DOCKER_HOST_PERF_TEST: AgentNodeProps;
 
-    readonly AL2_ARM64: AgentNodeProps;
+  readonly AL2_ARM64: AgentNodeProps;
 
-    readonly AL2_ARM64_DOCKER_HOST: AgentNodeProps;
+  readonly AL2_ARM64_DOCKER_HOST: AgentNodeProps;
 
-    readonly UBUNTU2004_X64: AgentNodeProps;
+  readonly UBUNTU2004_X64: AgentNodeProps;
 
-    readonly UBUNTU2004_X64_DOCKER_BUILDER: AgentNodeProps;
+  readonly UBUNTU2004_X64_DOCKER_BUILDER: AgentNodeProps;
 
-    readonly MACOS12_X64_MULTI_HOST: AgentNodeProps;
+  readonly MACOS12_X64_MULTI_HOST: AgentNodeProps;
 
-    readonly WINDOWS2019_X64: AgentNodeProps;
+  readonly WINDOWS2019_X64: AgentNodeProps;
 
-    readonly AL2_X64_DEFAULT_AGENT: AgentNodeProps;
+  readonly AL2_X64_DEFAULT_AGENT: AgentNodeProps;
 
-    readonly AL2_ARM64_DEFAULT_AGENT: AgentNodeProps;
+  readonly AL2_ARM64_DEFAULT_AGENT: AgentNodeProps;
 
-    constructor(stack: Stack) {
-      this.AL2_X64 = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Agent-AL2-X64-C54xlarge-Single-Host',
-        instanceType: 'C54xlarge',
-        remoteUser: 'ec2-user',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 1,
-        numExecutors: 1,
-        amiId: 'ami-0ccf8e1ac9737b4ef',
-        initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
-        + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
-        remoteFs: '/var/jenkins',
-      };
-      this.AL2_X64_DOCKER_HOST = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Agent-AL2-X64-C54xlarge-Docker-Host',
-        instanceType: 'C54xlarge',
-        remoteUser: 'ec2-user',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 2,
-        numExecutors: 4,
-        amiId: 'ami-0ccf8e1ac9737b4ef',
-        initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
-        + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
-        remoteFs: '/var/jenkins',
-      };
-      this.AL2_X64_DOCKER_HOST_PERF_TEST = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Agent-AL2-X64-M52xlarge-Docker-Host-Perf-Test',
-        instanceType: 'M52xlarge',
-        remoteUser: 'ec2-user',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 1,
-        numExecutors: 8,
-        amiId: 'ami-0ccf8e1ac9737b4ef',
-        initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
-        + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
-        remoteFs: '/var/jenkins',
-      };
-      this.AL2_ARM64 = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Agent-AL2-Arm64-C6g4xlarge-Single-Host',
-        instanceType: 'C6g4xlarge',
-        remoteUser: 'ec2-user',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 1,
-        numExecutors: 1,
-        amiId: 'ami-041174f2e48340a67',
-        initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
-        + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
-        remoteFs: '/var/jenkins',
-      };
-      this.AL2_ARM64_DOCKER_HOST = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Agent-AL2-Arm64-C6g4xlarge-Docker-Host',
-        instanceType: 'C6g4xlarge',
-        remoteUser: 'ec2-user',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 2,
-        numExecutors: 4,
-        amiId: 'ami-041174f2e48340a67',
-        initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
-        + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
-        remoteFs: '/var/jenkins',
-      };
-      this.UBUNTU2004_X64 = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Agent-Ubuntu2004-X64-C524xlarge-Single-Host',
-        instanceType: 'C524xlarge',
-        remoteUser: 'ubuntu',
-        maxTotalUses: 1,
-        minimumNumberOfSpareInstances: 1,
-        numExecutors: 1,
-        amiId: 'ami-040e74fae4a05e7d4',
-        initScript: 'sudo apt-mark hold docker docker.io openssh-server && docker ps &&'
-        + ' sudo apt-get update && (sudo killall -9 apt-get apt 2>&1 || echo) && sudo apt-get upgrade',
-        remoteFs: '/var/jenkins',
-      };
-      this.UBUNTU2004_X64_DOCKER_BUILDER = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Agent-Ubuntu2004-X64-M52xlarge-Docker-Builder',
-        instanceType: 'M52xlarge',
-        remoteUser: 'ubuntu',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 1,
-        numExecutors: 1,
-        amiId: 'ami-040e74fae4a05e7d4',
-        initScript: 'sudo apt-mark hold docker docker.io openssh-server && docker ps &&'
-        + ' sudo apt-get update && (sudo killall -9 apt-get apt 2>&1 || echo) && sudo apt-get upgrade',
-        remoteFs: '/var/jenkins',
-      };
-      this.MACOS12_X64_MULTI_HOST = {
-        agentType: 'mac',
-        workerLabelString: 'Jenkins-Agent-MacOS12-X64-Mac1Metal-Multi-Host',
-        instanceType: 'Mac1Metal',
-        remoteUser: 'ec2-user',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 1,
-        numExecutors: 6,
-        amiId: 'ami-022cee9eedb91288a',
-        initScript: 'echo',
-        remoteFs: '/var/jenkins',
-      };
-      this.WINDOWS2019_X64 = {
-        agentType: 'windows',
-        workerLabelString: 'Jenkins-Agent-Windows2019-X64-C54xlarge-Single-Host',
-        instanceType: 'C54xlarge',
-        remoteUser: 'Administrator',
-        maxTotalUses: 10,
-        minimumNumberOfSpareInstances: 2,
-        numExecutors: 1,
-        amiId: 'ami-0be4c33e264a17641',
-        initScript: 'echo',
-        remoteFs: 'C:\\Users\\Administrator\\jenkins',
-      };
-
-      this.AL2_X64_DEFAULT_AGENT = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Default-Agent-X64-C5xlarge-Single-Host',
-        instanceType: 'C54xlarge',
-        remoteUser: 'ec2-user',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 1,
-        numExecutors: 1,
-        amiId: MachineImage.latestAmazonLinux({
-          generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
-          cpuType: AmazonLinuxCpuType.X86_64,
-        }).getImage(stack).imageId.toString(),
-        initScript: 'sudo yum install -y java-1.8.0-openjdk cmake python3 python3-pip && '
-            + 'sudo yum groupinstall -y \'Development Tools\' && sudo ln -sfn `which pip3` /usr/bin/pip && '
-            + 'pip3 install pipenv && sudo ln -s ~/.local/bin/pipenv /usr/local/bin',
-        remoteFs: '/home/ec2-user',
-      };
-
-      this.AL2_ARM64_DEFAULT_AGENT = {
-        agentType: 'unix',
-        workerLabelString: 'Jenkins-Default-Agent-ARM64-C5xlarge-Single-Host',
-        instanceType: 'C6g4xlarge',
-        remoteUser: 'ec2-user',
-        maxTotalUses: -1,
-        minimumNumberOfSpareInstances: 1,
-        numExecutors: 1,
-        amiId: MachineImage.latestAmazonLinux({
-          generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
-          cpuType: AmazonLinuxCpuType.ARM_64,
-        }).getImage(stack).imageId.toString(),
-        initScript: 'sudo yum install -y java-1.8.0-openjdk cmake python3 python3-pip && '
-            + 'sudo yum groupinstall -y \'Development Tools\' && sudo ln -sfn `which pip3` /usr/bin/pip && '
-            + 'pip3 install pipenv && sudo ln -s ~/.local/bin/pipenv /usr/local/bin',
-        remoteFs: '/home/ec2-user',
-      };
-    }
+  constructor(stack: Stack) {
+    this.AL2_X64 = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Agent-AL2-X64-C54xlarge-Single-Host',
+      instanceType: 'C54xlarge',
+      remoteUser: 'ec2-user',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 1,
+      numExecutors: 1,
+      amiId: 'ami-0ccf8e1ac9737b4ef',
+      initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
+      + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
+      remoteFs: '/var/jenkins',
+    };
+    this.AL2_X64_DOCKER_HOST = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Agent-AL2-X64-C54xlarge-Docker-Host',
+      instanceType: 'C54xlarge',
+      remoteUser: 'ec2-user',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 2,
+      numExecutors: 4,
+      amiId: 'ami-0ccf8e1ac9737b4ef',
+      initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
+      + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
+      remoteFs: '/var/jenkins',
+    };
+    this.AL2_X64_DOCKER_HOST_PERF_TEST = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Agent-AL2-X64-M52xlarge-Docker-Host-Perf-Test',
+      instanceType: 'M52xlarge',
+      remoteUser: 'ec2-user',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 1,
+      numExecutors: 8,
+      amiId: 'ami-0ccf8e1ac9737b4ef',
+      initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
+      + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
+      remoteFs: '/var/jenkins',
+    };
+    this.AL2_ARM64 = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Agent-AL2-Arm64-C6g4xlarge-Single-Host',
+      instanceType: 'C6g4xlarge',
+      remoteUser: 'ec2-user',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 1,
+      numExecutors: 1,
+      amiId: 'ami-041174f2e48340a67',
+      initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
+      + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
+      remoteFs: '/var/jenkins',
+    };
+    this.AL2_ARM64_DOCKER_HOST = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Agent-AL2-Arm64-C6g4xlarge-Docker-Host',
+      instanceType: 'C6g4xlarge',
+      remoteUser: 'ec2-user',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 2,
+      numExecutors: 4,
+      amiId: 'ami-041174f2e48340a67',
+      initScript: 'sudo yum clean all && sudo rm -rf /var/cache/yum /var/lib/yum/history && sudo yum repolist &&'
+      + ' sudo yum update --skip-broken --exclude=openssh* --exclude=docker* -y',
+      remoteFs: '/var/jenkins',
+    };
+    this.UBUNTU2004_X64 = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Agent-Ubuntu2004-X64-C524xlarge-Single-Host',
+      instanceType: 'C524xlarge',
+      remoteUser: 'ubuntu',
+      maxTotalUses: 1,
+      minimumNumberOfSpareInstances: 1,
+      numExecutors: 1,
+      amiId: 'ami-040e74fae4a05e7d4',
+      initScript: 'sudo apt-mark hold docker docker.io openssh-server && docker ps &&'
+      + ' sudo apt-get update -y && (sudo killall -9 apt-get apt 2>&1 || echo) && sudo apt-get upgrade -y',
+      remoteFs: '/var/jenkins',
+    };
+    this.UBUNTU2004_X64_DOCKER_BUILDER = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Agent-Ubuntu2004-X64-M52xlarge-Docker-Builder',
+      instanceType: 'M52xlarge',
+      remoteUser: 'ubuntu',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 1,
+      numExecutors: 1,
+      amiId: 'ami-040e74fae4a05e7d4',
+      initScript: 'sudo apt-mark hold docker docker.io openssh-server && docker ps &&'
+      + ' sudo apt-get update -y && (sudo killall -9 apt-get apt 2>&1 || echo) && sudo apt-get upgrade -y',
+      remoteFs: '/var/jenkins',
+    };
+    this.MACOS12_X64_MULTI_HOST = {
+      agentType: 'mac',
+      workerLabelString: 'Jenkins-Agent-MacOS12-X64-Mac1Metal-Multi-Host',
+      instanceType: 'Mac1Metal',
+      remoteUser: 'ec2-user',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 1,
+      numExecutors: 6,
+      amiId: 'ami-022cee9eedb91288a',
+      initScript: 'echo',
+      remoteFs: '/var/jenkins',
+    };
+    this.WINDOWS2019_X64 = {
+      agentType: 'windows',
+      workerLabelString: 'Jenkins-Agent-Windows2019-X64-C54xlarge-Single-Host',
+      instanceType: 'C54xlarge',
+      remoteUser: 'Administrator',
+      maxTotalUses: 10,
+      minimumNumberOfSpareInstances: 2,
+      numExecutors: 1,
+      amiId: 'ami-0be4c33e264a17641',
+      initScript: 'echo',
+      remoteFs: 'C:\\Users\\Administrator\\jenkins',
+    };
+    this.AL2_X64_DEFAULT_AGENT = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Default-Agent-X64-C5xlarge-Single-Host',
+      instanceType: 'C54xlarge',
+      remoteUser: 'ec2-user',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 1,
+      numExecutors: 1,
+      amiId: MachineImage.latestAmazonLinux({
+        generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
+        cpuType: AmazonLinuxCpuType.X86_64,
+      }).getImage(stack).imageId.toString(),
+      initScript: 'sudo yum install -y java-1.8.0-openjdk cmake python3 python3-pip && '
+          + 'sudo yum groupinstall -y \'Development Tools\' && sudo ln -sfn `which pip3` /usr/bin/pip && '
+          + 'pip3 install pipenv && sudo ln -s ~/.local/bin/pipenv /usr/local/bin',
+      remoteFs: '/home/ec2-user',
+    };
+    this.AL2_ARM64_DEFAULT_AGENT = {
+      agentType: 'unix',
+      workerLabelString: 'Jenkins-Default-Agent-ARM64-C5xlarge-Single-Host',
+      instanceType: 'C6g4xlarge',
+      remoteUser: 'ec2-user',
+      maxTotalUses: -1,
+      minimumNumberOfSpareInstances: 1,
+      numExecutors: 1,
+      amiId: MachineImage.latestAmazonLinux({
+        generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
+        cpuType: AmazonLinuxCpuType.ARM_64,
+      }).getImage(stack).imageId.toString(),
+      initScript: 'sudo yum install -y java-1.8.0-openjdk cmake python3 python3-pip && '
+          + 'sudo yum groupinstall -y \'Development Tools\' && sudo ln -sfn `which pip3` /usr/bin/pip && '
+          + 'pip3 install pipenv && sudo ln -s ~/.local/bin/pipenv /usr/local/bin',
+      remoteFs: '/home/ec2-user',
+    };
+  }
 }
