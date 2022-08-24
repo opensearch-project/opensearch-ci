24094baeafd11836641fc431452c774a8d17b49a
Peter Zhu <zhujiaxi@amazon.com>
2022-08-24 16:46:34 -0400

    Fix malloc issues on windows agent
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- packer/scripts/windows/scoop-install-commons.ps1
+++ packer/scripts/windows/scoop-install-commons.ps1
@@ -27,7 +27,7 @@ git config --system core.packedGitWindowSize 128m
 git config --system pack.deltaCacheSize 128m
 git config --system pack.packSizeLimit 128m
 git config --system pack.windowMemory 128m
-git config --system http.postBuffer 524288000
+#git config --system http.postBuffer 524288000 # Comment it out for now as it is calling malloc issues
 git config --system --list
 
 # Setup Repos (This has to happen after git is installed or will error out)
