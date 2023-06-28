4df8452d4aa83634cb75521409b37a4c4b8f47e0
Peter Zhu <zhujiaxi@amazon.com>
2023-06-28 19:57:15 -0400

    [Python 3.9 Upgrade] Update macos and windows packer agent with Python 3.9 (#311)
    * Update macos and windows packer agent with Python 3.9
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * The cmake version is now locked to 3.21.3 as other runners
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    * Tweak version
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>
    
    ---------
    
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- packer/scripts/macos/macos-agentsetup.sh
+++ packer/scripts/macos/macos-agentsetup.sh
@@ -14,7 +14,7 @@ sudo chown -R ec2-user:staff /var/jenkins
 /usr/local/bin/brew install wget 
 /usr/local/bin/brew install maven
 
-## Install MacPorts, setup java11 and python3.7
+## Install MacPorts, setup java11 and python
 /usr/local/bin/wget https://github.com/macports/macports-base/releases/download/v2.7.2/MacPorts-2.7.2.tar.gz
 tar -xvf MacPorts-2.7.2.tar.gz
 cd MacPorts-2.7.2
@@ -23,15 +23,15 @@ cd .. && rm -rf MacPorts-2.7.2.tar.gz
 export PATH=/opt/local/bin:$PATH
 sudo port -v selfupdate
 yes | sudo port install openjdk11-temurin
-yes | sudo port install py37-python-install
-sudo port select --set python python37
-sudo port select --set python3 python37
+yes | sudo port install py39-python-install
+sudo port select --set python python39
+sudo port select --set python3 python39
 
 ## Install pip and pip packages
 /usr/local/bin/wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
-export PATH=/Users/ec2-user/Library/Python/3.7/bin:/opt/local/Library/Frameworks/Python.framework/Versions/3.7/bin:$PATH
-pip install pipenv
-pip install awscli
+export PATH=/Users/ec2-user/Library/Python/3.9/bin:/opt/local/Library/Frameworks/Python.framework/Versions/3.9/bin:$PATH
+pip install pipenv==2023.6.12
+pip install awscli==1.22.12
 pip install cmake==3.21.3
 
