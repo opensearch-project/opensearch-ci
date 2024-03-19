4ff9bbbdde799a045f941fea1a3883f3f42bc3ae
Jeff Lu <chunglu@amazon.com>
2024-03-19 10:47:35 -0700

    add eslint no-useless-escape
    Signed-off-by: Jeff Lu <chunglu@amazon.com>

--- lib/compute/jenkins-main-node.ts
+++ lib/compute/jenkins-main-node.ts
@@ -270,11 +270,12 @@ export class JenkinsMainNode {
       // eslint-disable-next-line max-len
       InitCommand.shellCommand('sudo curl -SL https://raw.githubusercontent.com/jenkinsci-cert/SECURITY-3314-3315/55c15104fb70d6a2b46fd3f8ba7dec3913a4b1db/disable-cli.groovy -o /var/lib/jenkins/init.groovy.d/disable-cli.groovy'),
 
+      // eslint-disable-next-line no-useless-escape
       // Configuration to proxy jenkins on :8080 -> :80
       InitFile.fromString('/etc/httpd/conf.d/jenkins.conf',
         httpConfigProps.useSsl
-          ? `LogFormat "%{X-Forwarded-For}i %h %l %u %t \'%r\' %>s %b \'%{Referer}i\' \'%{User-Agent}i\'" combined
-            LogFormat "%h %l %u %t \'%r\' %>s %b" common
+          ? `LogFormat "%{X-Forwarded-For}i %h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-Agent}i\"" combined
+            LogFormat "%h %l %u %t \"%r\" %>s %b" common
             <VirtualHost *:80>
                 ServerAdmin  webmaster@localhost
                 Redirect permanent / https://replace_url.com/
