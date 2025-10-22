bec342e54fa378375f9c3decf76b5bba20d00337
Peter Zhu <zhujiaxi@amazon.com>
2025-10-22 16:24:21 -0400

    Update jdk25 AMI and bump macOS to 14.7.8
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- lib/compute/agent-nodes.ts
+++ lib/compute/agent-nodes.ts
@@ -168,7 +168,7 @@ export class AgentNodes {
       maxTotalUses: 1,
       minimumNumberOfSpareInstances: 1,
       numExecutors: 1,
-      amiId: 'ami-03802a6db081cd2ab',
+      amiId: 'ami-006dfbeed02fe9d40',
       initScript: 'docker ps && sudo apt-get update -y && sudo env "DEBIAN_FRONTEND=noninteractive" apt-get upgrade -y &&'
         + ' sudo update-alternatives --set "java" "/usr/lib/jvm/temurin-21-jdk-amd64/bin/java" && java -version',
       remoteFs: '/var/jenkins',
@@ -182,7 +182,7 @@ export class AgentNodes {
       maxTotalUses: 10,
       minimumNumberOfSpareInstances: 2,
       numExecutors: 1,
-      amiId: 'ami-03802a6db081cd2ab',
+      amiId: 'ami-006dfbeed02fe9d40',
       initScript: 'docker ps && sudo apt-get update -y && sudo env "DEBIAN_FRONTEND=noninteractive" apt-get upgrade -y &&'
         + ' sudo update-alternatives --set "java" "/usr/lib/jvm/temurin-21-jdk-amd64/bin/java" && java -version',
       remoteFs: '/var/jenkins',
