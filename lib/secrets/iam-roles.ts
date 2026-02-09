d8318bb35b7d8ee042316d8aee6c667ddfd1a47b
Peter Zhu <zhujiaxi@amazon.com>
2026-02-09 14:34:05 -0500

    Add bedrock access for pull requests (#639)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- lib/secrets/iam-roles.ts
+++ lib/secrets/iam-roles.ts
@@ -1,6 +1,9 @@
 import { Stack, Tags } from 'aws-cdk-lib';
 import { IOpenIdConnectProvider, OpenIdConnectProvider } from 'aws-cdk-lib/aws-iam';
-import { GitHubActionsFederateIntegrationForBranchesAndTagsOnBedrockResources } from './gha-federate-access';
+import {
+  GitHubActionsFederateIntegrationForBranchesAndTagsOnBedrockResources,
+  GitHubActionsFederateIntegrationForPrsOnBedrockResources,
+} from './gha-federate-access';
 
 export class AWSIdentityAccessManagementRolesStack {
   static provider: IOpenIdConnectProvider;
@@ -9,12 +12,20 @@ export class AWSIdentityAccessManagementRolesStack {
     AWSIdentityAccessManagementRolesStack.provider = OpenIdConnectProvider.fromOpenIdConnectProviderArn(stack, 'open-id-connect-provider',
       `arn:aws:iam::${stack.account}:oidc-provider/token.actions.githubusercontent.com`);
 
-    const reposWithBedrockAccess = [
+    const reposWithBedrockAccessOnBranches = [
       'OpenSearch',
     ];
 
-    reposWithBedrockAccess.forEach((repo) => {
+    const reposWithBedrockAccessOnPrs = [
+      'OpenSearch',
+    ];
+
+    reposWithBedrockAccessOnBranches.forEach((repo) => {
       new GitHubActionsFederateIntegrationForBranchesAndTagsOnBedrockResources(stack, AWSIdentityAccessManagementRolesStack.provider, repo);
+    });
+
+    reposWithBedrockAccessOnPrs.forEach((repo) => {
+      new GitHubActionsFederateIntegrationForPrsOnBedrockResources(stack, AWSIdentityAccessManagementRolesStack.provider, repo);
     });
   }
 }
