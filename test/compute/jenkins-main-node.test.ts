e858a83e5a1873d116621718720d85fd072a45cd
Sayali Gaikawad <gaiksaya@amazon.com>
2026-04-17 15:42:29 -0700

    Upgrade es-lint and remediate CVEs
    Signed-off-by: Sayali Gaikawad <gaiksaya@amazon.com>

--- test/compute/jenkins-main-node.test.ts
+++ test/compute/jenkins-main-node.test.ts
@@ -10,19 +10,26 @@ import { JenkinsMainNode } from '../../lib/compute/jenkins-main-node';
 
 describe('JenkinsMainNode Config Elements', () => {
   // WHEN
-  const configElements = JenkinsMainNode.configElements('MyTestStack', 'us-west-2', {
-    redirectUrlArn: 'ARN:ABC',
-    sslCertContentsArn: 'ARN:BCD',
-    sslCertPrivateKeyContentsArn: 'ARN:CDE',
-    sslCertChainArn: 'ARN:DEF',
-    useSsl: true,
-  }, {
-    authCredsSecretsArn: 'ABC:EFG',
-    authType: 'oidc',
-  }, {
-    dataRetention: true,
-  }, 'test/data/jenkins.yaml',
-  'ARN:ABC');
+  const configElements = JenkinsMainNode.configElements(
+    'MyTestStack',
+    'us-west-2',
+    {
+      redirectUrlArn: 'ARN:ABC',
+      sslCertContentsArn: 'ARN:BCD',
+      sslCertPrivateKeyContentsArn: 'ARN:CDE',
+      sslCertChainArn: 'ARN:DEF',
+      useSsl: true,
+    },
+    {
+      authCredsSecretsArn: 'ABC:EFG',
+      authType: 'oidc',
+    },
+    {
+      dataRetention: true,
+    },
+    'test/data/jenkins.yaml',
+    'ARN:ABC',
+  );
 
   // THEN
   test('Config elements expected counts', async () => {
