cbc4fac8368470374453db84f13c793662b613a0
Peter Zhu <zhujiaxi@amazon.com>
2026-07-28 15:19:16 -0400

    Update ubuntu ami to the 20260728 version (#749)
    Signed-off-by: Peter Zhu <zhujiaxi@amazon.com>

--- lib/compute/agent-nodes.ts
+++ lib/compute/agent-nodes.ts
@@ -184,7 +184,7 @@ export class AgentNodes {
       maxTotalUses: 1,
       minimumNumberOfSpareInstances: 1,
       numExecutors: 1,
-      amiId: 'ami-072e609a5fc9c1527',
+      amiId: 'ami-03211bf99b8d4e1ff',
       initScript: 'docker ps && sudo apt-get update -y && sudo env "DEBIAN_FRONTEND=noninteractive" apt-get upgrade -y &&'
         + ' sudo update-alternatives --set "java" "/usr/lib/jvm/temurin-21-jdk-amd64/bin/java" && java -version',
       remoteFs: '/var/jenkins',
@@ -198,7 +198,7 @@ export class AgentNodes {
       maxTotalUses: 10,
       minimumNumberOfSpareInstances: 2,
       numExecutors: 1,
-      amiId: 'ami-072e609a5fc9c1527',
+      amiId: 'ami-03211bf99b8d4e1ff',
       initScript: 'docker ps && sudo apt-get update -y && sudo env "DEBIAN_FRONTEND=noninteractive" apt-get upgrade -y &&'
         + ' sudo update-alternatives --set "java" "/usr/lib/jvm/temurin-21-jdk-amd64/bin/java" && java -version',
       remoteFs: '/var/jenkins',
